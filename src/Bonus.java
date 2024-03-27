2e8d4c85e416a64701d5def0c3cda04197169918
TubaGuneri <t_guneri@hotmail.com>
2024-03-27 19:59:56 +0100

    opdracht klaar

--- src/Bonus.java
+++ src/Bonus.java
@@ -1,5 +1,6 @@
 import java.util.HashSet;
 import java.util.Objects;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Bonus {
@@ -12,12 +13,13 @@ public class Bonus {
 
         HashSet<Integer> secretnumber = randomnumbergenerator();
         String stringnumber = setToStringConverter(secretnumber);
-        System.out.println(stringnumber);
-        feedback(/*vul hier het juiste argument in*/);
+//        System.out.println(stringnumber);
+        feedback(stringnumber);
+//        System.out.println(secretnumber);
 
     }
 
-    public static void/*moet dit returntype "void" zijn of wat anders?*/ randomnumbergenerator(/*Heeft deze methode nog parameter(s) nodig?*/){
+    public static HashSet<Integer> randomnumbergenerator(/*Heeft deze methode nog parameter(s) nodig?*/) {
          /*
         Vul hier de body van de methode in.
 
@@ -27,9 +29,15 @@ public class Bonus {
         - Schrijf een while-loop om 4 random nummers aan de hashset toe te voegen
         - return de hashset
          */
+        Random random = new Random();
+        HashSet<Integer> hashSet = new HashSet<>();
+        while (hashSet.size() < 4) {
+            hashSet.add(random.nextInt(10));
+        }
+        return hashSet;
     }
 
-    public static void/*moet dit returntype "void" zijn of wat anders?*/ setToStringConverter(/*Heeft deze methode nog parameter(s) nodig?*/){
+    public static String setToStringConverter(HashSet<Integer> hashSet) {
         /*
         Vul hier de body van de methode in.
 
@@ -38,29 +46,39 @@ public class Bonus {
         - Schrijf vervolgens een for-loop om de items in de hashset een voor een aan de String variabele toe te voegen.
         - Return de (gevulde) String variabele
          */
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Integer number : hashSet) {
+            stringBuilder.append(number);
+        }
+        return stringBuilder.toString();
     }
 
 
-
-    public static void/*moet dit "void" zijn of wat anders?*/ feedback(String stringnumber) {
+    public static void feedback(String stringnumber) {
         Scanner scanner = new Scanner(System.in);
         StringBuilder feedback = new StringBuilder();
+        Boolean isCorrect = false;
         System.out.println("+ = juiste nummer op de juiste plek, O = juiste nummer verkeerde plek, X = verkeerde nummer");
         System.out.println("Doe een gok, Let op vul 4 getallen in.");
-        String guess = scanner.nextLine();
-        if (Objects.equals(guess, stringnumber)) {
-            System.out.println("gefeliciteerd je hebt het goed");
-        } else {
-            for (int i = 0; i < 4; i++) {
-                if (guess.substring(i, i + 1).equals(stringnumber.substring(i, i + 1))) {
-                    feedback.append("+");
-                } else if (stringnumber.contains(guess.substring(i, i + 1))) {
-                    feedback.append("0");
-                } else {
-                    feedback.append("X");
+        while (!isCorrect) {
+            String guess = scanner.nextLine();
+            if (Objects.equals(guess, stringnumber)) {
+                System.out.println("gefeliciteerd je hebt het goed");
+                isCorrect = true;
+            } else {
+                for (int i = 0; i < 4; i++) {
+                    if (guess.substring(i, i + 1).equals(stringnumber.substring(i, i + 1))) {
+                        feedback.append("+");
+                    } else if (stringnumber.contains(guess.substring(i, i + 1))) {
+                        feedback.append("0");
+                    } else {
+                        feedback.append("X");
+                    }
                 }
             }
+            System.out.println(feedback.toString());
+        feedback.delete(0,4);
         }
-        System.out.println(feedback.toString());
     }
 }
